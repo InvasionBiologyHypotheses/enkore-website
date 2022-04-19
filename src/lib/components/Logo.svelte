f2b0b4e0707d519e7c152adbe8916c1acdff0e82
Steph Tyszka <steph@bootsa.net>
2022-04-19 14:39:40 +0200

    add Logo component

---
+++ src/lib/components/Logo.svelte
@@ -0,0 +1,12 @@
+<div>enKORE</div>
+
+<style>
+	div {
+		line-height: var(--font-lineheight-1);
+		font-size: var(--font-size-fluid-2);
+		font-weight: var(--font-weight-6);
+		font-family: var(--font-sans);
+		color: var(--brand);
+		padding-inline: var(--size-3);
+	}
+</style>
