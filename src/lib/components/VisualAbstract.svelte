2a6b482319451ba7c3dc93d7bd23529e6c236b68
Steph Tyszka <steph@bootsa.net>
2022-04-24 00:17:23 +0200

    Implement Visual Abstract as vector graphic (SVG) Fixes #6

---
+++ src/lib/components/VisualAbstract.svelte
@@ -0,0 +1,113 @@
+<!-- prettier-ignore -->
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 200 1684 1064">
+  <defs>
+    <marker id="arrowhead-involvement" orient="auto" overflow="visible" refX="0" refY="0">
+      <path fill="#ff3c36" fill-rule="evenodd" stroke="none" stroke-linejoin="round" stroke-width=".4" d="m-5-2 6 2-6 2v-4z"/>
+    </marker>
+    <marker id="arrowhead-process" orient="auto" overflow="visible" refX="0" refY="0">
+    <path id="arrow" fill="black" fill-rule="evenodd" stroke="none" d="m-2-1 3 1-3 1v-2z"/>
+    </marker>
+  </defs>
+
+	<g id="process">
+
+		<g id="participants">
+			<rect id="rect42389" ry="45" rx="45" fill="none" stroke="black" stroke-width="10" y="817" x="590" width="340" height="225" />
+
+			<path fill-rule="evenodd" d="M214 972a3 3 0 0 0-2-1 3 3 0 0 0-4 3l1 3a3 3 0 0 0 2 1h1a3 3 0 0 0 2-1 3 3 0 0 0 0-5m-19 31a4 4 0 0 0-3-1 3 3 0 0 0-2 1 3 3 0 0 0 0 5 3 3 0 0 0 2 1 3 3 0 0 0 4-3 3 3 0 0 0-1-3zm30 22a4 4 0 0 0-5 0 3 3 0 0 0-1 3l1 2a3 3 0 0 0 2 1 3 3 0 0 0 4-3l-1-3zm-57 11a4 4 0 0 0-5 0 3 3 0 0 0-1 2 3 3 0 0 0 4 4 3 3 0 0 0 3-4 3 3 0 0 0-1-2zm-17-36 5 2a8 8 0 0 1 3 6l-3 6a8 8 0 0 1-5 2 8 8 0 0 1-6-2 8 8 0 0 1-2-6l2-6a8 8 0 0 1 6-2zm15 32 4 2a6 6 0 0 1 2 4l-2 5a7 7 0 0 1-3 1v14l14 24v-95a1 1 0 0 1 1-1 1 1 0 0 1 2 1v67l7-12v-30a6 6 0 0 1-3-2 6 6 0 0 1-2-4l2-5a6 6 0 0 1 4-1l4 1a6 6 0 0 1 2 5 6 6 0 0 1-2 4 6 6 0 0 1-3 2v30a1 1 0 0 1 0 1l-9 16v49h26v-87l-13-22a1 1 0 0 1 0-1v-28a1 1 0 0 1 2-1l1 1v28l10 18v-36a6 6 0 0 1-3-1 6 6 0 0 1-1-5 6 6 0 0 1 1-4 6 6 0 0 1 5-2l4 2a6 6 0 0 1 0 9 6 6 0 0 1-3 1v103l8-8v-41a6 6 0 0 1-3-2 6 6 0 0 1-2-4l2-5a6 6 0 0 1 4-1 6 6 0 0 1 5 1 6 6 0 0 1 1 5l-1 4a6 6 0 0 1-3 2v41l-1 1-10 10v22h18l3-46 1-11 4-8c11-12 18-27 18-41 1-14-4-28-14-39a69 69 0 0 0-83-5c-25 18-25 37-25 50 1 6 1 11-2 15l-6 8c-3 3-5 5-5 7-1 2 1 3 6 4h1l2 1c4 1 4 7 3 13v6c0 6 2 11 11 11l4-1c6 0 11 0 14 4l3 5a33 33 0 0 1 0 7l-1 20h16v-21a1 1 0 0 1 0-1l-16-27a1 1 0 0 1 0-1v-14a6 6 0 0 1-3-1 6 6 0 0 1-2-5c0-1 0-3 2-4a6 6 0 0 1 4-2zm-46 85a1 1 0 1 1 0-3h100a1 1 0 0 1 0 3zm58 7a1 1 0 1 1 0-3h69a1 1 0 0 1 0 3zm76 0a1 1 0 0 1 0-3h9a1 1 0 0 1 0 3zm-138-1a1 1 0 1 1 0-2h33a1 1 0 1 1 0 2zm13-103a1 1 0 0 1-2-2c2-2 2-7 2-13-1-14-1-33 25-52 6-4 13-8 21-10a75 75 0 0 1 24-2 1 1 0 0 1 2 1 1 1 0 0 1-2 2 63 63 0 0 0-44 12c-24 17-24 36-24 49 0 6 0 11-2 15zm26-16a5 5 0 0 0-8 0 5 5 0 0 0 0 8 5 5 0 0 0 8 0 5 5 0 0 0 0-8z"/>
+			<text x="204" y="1163" fill="#1a1a1a" font-family="OpenSans" font-size="28" font-weight="700" text-anchor="middle">
+				<tspan x="204" y="1163" text-anchor="middle">Data scientists</tspan>
+			</text>
+
+			<g>
+					<path d="M240 670a57 57 0 0 0-40-17 57 57 0 0 0-21 3 57 57 0 0 0-18 11 57 57 0 0 0-19 43v5l-19 28a4 4 0 0 0 0 2v2a4 4 0 0 0 2 2 4 4 0 0 0 2 1h15v23a12 12 0 0 0 4 9 12 12 0 0 0 8 3h19v38a4 4 0 0 0 1 3 4 4 0 0 0 3 1 4 4 0 0 0 3-1 4 4 0 0 0 1-3v-42a4 4 0 0 0-1-3 4 4 0 0 0-3-1h-23a3 3 0 0 1-2-1 3 3 0 0 1-1-3v-28a4 4 0 0 0-1-3 4 4 0 0 0-3-1h-12l15-22a4 4 0 0 0 1-3v-6c0-14 6-27 16-37a48 48 0 0 1 15-9 48 48 0 0 1 18-3c26 0 48 23 48 49a47 47 0 0 1-6 22 84 84 0 0 0-7 19 84 84 0 0 0-2 20v52a4 4 0 0 0 1 3 4 4 0 0 0 3 1 4 4 0 0 0 3-1 4 4 0 0 0 1-3v-52a76 76 0 0 1 2-18 76 76 0 0 1 7-17 56 56 0 0 0 5-13 56 56 0 0 0 1-13c0-15-5-29-16-40z"/>
+					<path d="M203 701a4 4 0 0 0-3-1 4 4 0 0 0-3 1l-26 26a4 4 0 0 0-1 3 4 4 0 0 0 1 3 4 4 0 0 0 3 2 4 4 0 0 0 3-2l26-26a4 4 0 0 0 2-3 4 4 0 0 0-2-3z"/>
+					<path d="M224 685a4 4 0 0 0-2-3 4 4 0 0 0-3-1l-28 6a23 23 0 0 0-12 6 23 23 0 0 0-3 4 23 23 0 0 0-3 14 23 23 0 0 0 0 5 4 4 0 0 0 2 2 4 4 0 0 0 4 1 4 4 0 0 0 2-2 4 4 0 0 0 0-4 15 15 0 0 1 0-6 15 15 0 0 1 2-5 14 14 0 0 1 4-5 14 14 0 0 1 6-2l21-4-4 21a14 14 0 0 1-2 6 14 14 0 0 1-5 4 15 15 0 0 1-5 2l-7-1a4 4 0 0 0-3 1 4 4 0 0 0-2 2 4 4 0 0 0 1 3 4 4 0 0 0 2 3 23 23 0 0 0 7 0 23 23 0 0 0 12-3 23 23 0 0 0 7-7 23 23 0 0 0 3-8l5-27 1-2z"/>
+				</g>
+			<text x="204" y="867" fill="#1a1a1a" font-family="OpenSans" font-size="28" font-weight="700"  text-anchor="middle">
+				<tspan x="204" y="867">Invasion biologists</tspan>
+			</text>
+		</g>
+
+		<g id="arrow-participants-projects">
+			<path fill="none" stroke="#000" stroke-width="8" d="M348 784h81v143h127"/>
+			<path fill="none" stroke="#000" stroke-width="8" marker-end="url(#arrowhead-process)" d="M348 1062h81V927h140"/>
+		</g>
+
+		<g id="projects">
+			<text x="760" y="909" fill="#1a1a1a" font-family="Bangers" font-size="56" font-weight="400" text-anchor="middle">
+				<tspan x="760">enKore/INAS</tspan>
+				<tspan x="760" dy="1.25em">Projects</tspan>
+			</text>
+		</g>
+
+		<path id="link-projects-output" fill="none" stroke="#000" stroke-width="8" marker-end="url(#arrowhead-process)" d="M940 927h99"/>
+
+		<g id="output">
+			<g>
+				<text x="1280" y="750" fill="#1a1a1a" stroke-width="1.3" font-family="OpenSans" font-size="32" font-weight="700" text-anchor="middle">
+						<tspan>Atlas of Knowledge</tspan>
+				</text>
+				<text x="1280" y="789" fill="#1a1a1a" font-family="OpenSans" font-size="22" font-weight="700" text-anchor="middle">
+						<tspan>for Invasion Biology and beyond!</tspan>
+				</text>
+			</g>
+			<rect width="445.2" height="493.9" x="1058" y="680" fill="none" stroke="#000" stroke-dasharray="10 10" stroke-linejoin="round" stroke-width="9.8" rx="90" ry="90"/>
+		</g>
+
+	</g>
+
+	<g id="participant">
+		<g id="icon">
+			<g>
+				<path fill="#47080" d="M733 386h20l16 15 15-15h20a3 3 0 0 0 3-3v-48a3 3 0 0 0-3-3h-71a3 3 0 0 0-3 3v48c0 2 1 3 3 3zm3-48h65v42h-20l-12 13-13-13h-20z"/>
+				<path fill="#47080" d="M817 369a3 3 0 0 0-4-1 3 3 0 0 0-1 4 48 48 0 0 1 5 20 47 47 0 0 1-4 19c-6 14-9 27-9 41v10l-1 1-58 12v-8l-1-11-1-2h-12l-7-1-2-4-4-28v-1h-1l-1-1a6 6 0 0 0-2-1l-3-1-3-2a7 7 0 0 1 1-2l5-5 2-2c3-2 5-5 6-8v-5a46 46 0 0 1 3-19c1-1 0-3-1-4-2 0-3 0-4 2a53 53 0 0 0-4 20v5l-4 5-2 2-6 6-2 3c-1 3 0 5 3 7a35 35 0 0 0 4 2 109 109 0 0 1 3 1 4785 4785 0 0 1 4 26c0 4 2 6 3 8 4 3 8 3 12 3h7l1 7 1 9a5 5 0 0 0 1 4 5 5 0 0 0 3 1h1l59-12c4-1 6-3 6-7v-11c0-12 3-25 8-37 3-7 5-15 4-23a53 53 0 0 0-5-22z"/>
+				<path fill="#447080" d="M768 370a3 3 0 0 0-3 3 3 3 0 0 0 3 3l2-1a3 3 0 0 0 0-2 3 3 0 0 0-2-3zm-7-18c1 1 3 0 3-1 1-2 2-3 4-3s4 0 4 2v3l-3 3-3 3-1 6a3 3 0 0 0 3 2 3 3 0 0 0 2-2l1-4 1-1 4-3c2-3 3-7 1-10-1-3-5-5-9-5s-7 3-9 6a3 3 0 0 0 2 4Z"/>
+			</g>
+
+			<text x="759" y="441" fill="#447080" font-family="Caveat" font-size="38" font-weight="400" text-anchor="middle">
+				<tspan x="759" y="441">You!</tspan>
+			</text>
+		</g>
+
+		<g id="interests">
+			<text x="1300" y="278" fill="#447080" font-family="Caveat" font-size="38" font-weight="400" text-anchor="middle">
+				<tspan x="1050" y="278">Meta-analysis project?</tspan>
+				<tspan x="1050" y="326">New research topic?</tspan>
+				<tspan x="1050" y="374">Catching up on literature?</tspan>
+			</text>
+		</g>
+	</g>
+
+
+	<g id="involvement">
+		<g id="involvement-text" fill="#ff3c36" font-family="sans-serif" font-size="36" font-weight="400" style="text-align:center" text-anchor="middle">
+			<text id="involvement-text-1" x="350" y="475">
+				<tspan>Join the team!</tspan>
+			</text>
+			<text x="780" y="670">
+				<tspan>Provide expertise</tspan>
+			</text>
+			<text x="1100" y="555">
+				<tspan x="1100" dx="0">Deﬁne needs</tspan>
+				<tspan x="1100" dy="1.5em">and wishes</tspan>
+			</text>
+			<text x="1350" y="470">
+				<tspan x="1350">Test new</tspan>
+				<tspan x="1450" dy="1.5em">interactive tools</tspan>
+			</text>
+		</g>
+		<g id="involvement-arrows" fill="none" stroke="#ff3c36" stroke-dasharray="1 8" stroke-linecap="round" stroke-miterlimit="2" stroke-width="4" marker-end="url(#arrowhead-involvement)">
+			<path d="M192 636c-2-205 329-229 491-230" clip-path="url(#involvement-text-1) fill-box"/>
+			<path d="M764 486v314" clip-path="url(#involvement-text-2)" />
+			<path d="M798 485c0 202 485-35 485 183"/>
+			<path d="M1517 925c180 9 302-565-674-519"/>
+		</g>
+	</g>
+</svg>
+
+<style>
+	svg {
+		width: var(--width, 'unset');
+	}
+</style>
